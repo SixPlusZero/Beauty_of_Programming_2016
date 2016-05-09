d8cd1b0839c3c9bfe99620938149400cfaa09887
sixpluszero <russellspurs@gmail.com>
2016-05-09 14:04:49 +0800

    rewrite_(4/4)part_of_2hop

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def wrapper_1hop(id1, id2):
 	global ret_list_1hop
 	ret_list_1hop = calc_1hop.calc(id1, id2)
 
+def wrapper_2hop(id1, id2):
+	global ret_list_1hop
+	ret_list_1hop = calc_2hop.main(id1, id2)
+
+
 def wrapper_3hop(id1, id2):
 	global ret_list_3hop
 	ret_list_3hop = calc_3hop.calc(id1, id2)
@@ -26,7 +31,10 @@ def request(arg1, arg2):
         return 'Error: Illegal arguments!'
     t_1hop = threading.Thread(target=wrapper_1hop,args=(id1, id2))
     t_1hop.start()
-    
+
+    t_2hop = threading.Thread(target=wrapper_2hop,args=(id1, id2))
+    t_2hop.start()
+
     t_3hop = threading.Thread(target=wrapper_3hop,args=(id1, id2))
     t_3hop.start()
 
